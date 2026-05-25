1220fee9f41e95d432c1d4abd2585c051ac7fac2
laborant <laborant@labs.iximiuz.com>
2026-05-25 19:26:17 +0000

    Revert "break the test"
    This reverts commit 27afc2201bd652de2a53d41700d4fc4652a61ee8.

--- main_test.go
+++ main_test.go
@@ -5,8 +5,7 @@ import "testing"
 func TestSimpleFactory(t *testing.T) {
     f := SimpleFactory("http://localhost")
     
-    if f.Url != "http://wrong-host-on-purpose" {
-
+    if f.Url != "http://localhost" {
         t.Errorf("feature incorrect, got %s, want: %s", f.Url, "http://localhost")
     }
 }

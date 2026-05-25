ae16575cb45d92733bc14117be3216c18a1a01f8
laborant <laborant@labs.iximiuz.com>
2026-05-25 19:43:43 +0000

    test

--- main_test.go
+++ main_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestSimpleFactory(t *testing.T) {
     f := SimpleFactory("http://localhost")
     
-    if f.Url != "http://localhost" {
+    if f.Url != "http://wrong-host-on-purpose" {
         t.Errorf("feature incorrect, got %s, want: %s", f.Url, "http://localhost")
     }
 }
